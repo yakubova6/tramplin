7ecb4e61267299b7d621589d10c4adc405f933f5
AlekseyIshchenko <oleks1o@yandex.ru>
2026-03-23 23:48:00 +0300

    feat/tramplin-005 Создал модуль commons и перенес туда UserDao + UserDto

---
+++ server/commons/src/test/kotlin/ru/itplanet/tramplin/commons/CommonsApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.itplanet.tramplin.commons
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommonsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
